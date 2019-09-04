2a3009d90d83e3a6f512c4c5108adf61cb2e9de5
Daniel Horne <d.horne92@live.co.uk>
2019-09-04 16:27:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def new_category():
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
+        port=int(process.env.PORT),
         debug=True)
